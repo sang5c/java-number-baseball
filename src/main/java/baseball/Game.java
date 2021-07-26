ec7e4f9e079bd9b111d26e5cfb5c070ff1dff939
sang5c <skah321@gmail.com>
2021-07-26 22:52:38 +0900

    refactor: 원시타입 제거, 메소드로 분리

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,28 +2,44 @@ package baseball;
 
 public class Game {
     public static final int GAME_LENGTH = 3;
-    private String str;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+    public static final String SPLIT_REGEX = "";
+    public static final String OUT_OF_RANGE_STR = "range should be 1-9, input: [%s]";
+    public static final String INVALID_LENGTH_STR = "length should be 3, input: [%d]";
+    private final String str;
 
-    public Game(String str) {
+    private Game(String str) {
         this.str = str;
     }
 
     public static Game of(String str) {
-        validateLength(str);
+        validate(str);
+        return new Game(str);
+    }
 
-        String[] split = str.split("");
+    private static void validate(String str) {
+        validateLength(str);
+        validateRange(str);
+    }
+
+    private static void validateRange(String str) {
+        String[] split = str.split(SPLIT_REGEX);
         for (String s : split) {
             int number = Integer.parseInt(s);
-            if (number < 1 || number > 9) {
-                throw new IllegalArgumentException(String.format("range should be 1-9, input: [%s]", s));
+            if (isOutOfRange(number)) {
+                throw new IllegalArgumentException(String.format(OUT_OF_RANGE_STR, s));
             }
         }
-        return new Game(str);
+    }
+
+    private static boolean isOutOfRange(int number) {
+        return number < MIN_NUMBER || number > MAX_NUMBER;
     }
 
     private static void validateLength(String str) {
         if (isValidLength(str))
-            throw new IllegalArgumentException(String.format("length should be 3, input: [%d]", str.length()));
+            throw new IllegalArgumentException(String.format(INVALID_LENGTH_STR, str.length()));
     }
 
     private static boolean isValidLength(String str) {
