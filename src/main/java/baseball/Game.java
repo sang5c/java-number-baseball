3c7af4799ea465aed68fdb2464f379847be27a65
sang5c <skah321@gmail.com>
2021-07-26 22:45:04 +0900

    feat: game 숫자의 범위가 1~9가 아니면 에러

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -10,6 +10,14 @@ public class Game {
 
     public static Game of(String str) {
         validateLength(str);
+
+        String[] split = str.split("");
+        for (String s : split) {
+            int number = Integer.parseInt(s);
+            if (number < 1 || number > 9) {
+                throw new IllegalArgumentException(String.format("range should be 1-9, input: [%s]", s));
+            }
+        }
         return new Game(str);
     }
 
