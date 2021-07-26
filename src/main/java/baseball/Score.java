03b59de2c6c83f91c99f8cfb695f14312a274d29
sang5c <skah321@gmail.com>
2021-07-27 04:00:33 +0900

    feat: 스트라이크와 볼이 같다면 같은 Score 객체를 반환한다.

--- src/main/java/baseball/Score.java
+++ src/main/java/baseball/Score.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.Objects;
+
 public class Score {
     private int strike;
     private int ball;
@@ -7,6 +9,19 @@ public class Score {
     public Score(int strike, int ball) {
         this.strike = strike;
         this.ball = ball;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Score score = (Score) o;
+        return strike == score.strike && ball == score.ball;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(strike, ball);
     }
 
 }
