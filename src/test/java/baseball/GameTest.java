624491a52af3faae376aa737ebe54f0220e9f25c
sang5c <skah321@gmail.com>
2021-07-26 22:02:04 +0900

    refactor: game 생성

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,11 +1,13 @@
 package baseball;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameTest {
 
+    @DisplayName("세자리 숫자를 받아 게임을 생성")
     @Test
     public void CreateGame() {
         Game game = Game.of("123");
