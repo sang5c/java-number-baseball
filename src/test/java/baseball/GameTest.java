0a0896a7af50c87321e83fd262af1d151d97e4bc
sang5c <skah321@gmail.com>
2021-07-26 22:11:13 +0900

    test: game 생성시 세자리가 아니면 에러

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -4,14 +4,21 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class GameTest {
 
     @DisplayName("세자리 숫자를 받아 게임을 생성")
     @Test
-    public void CreateGame() {
+    public void createGame() {
         Game game = Game.of("123");
         assertThat(game).isNotNull();
+    }
+
+    @DisplayName("세자리가 아니면 에러")
+    @Test
+    void invalidGameLength() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Game.of("1234"));
     }
 
 }
