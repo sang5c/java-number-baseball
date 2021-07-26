d625430d09578d8764305a1619cd7296837a21f5
sang5c <skah321@gmail.com>
2021-07-27 06:14:44 +0900

    feat(Number): 1-9 숫자가 아니면 Exception이 발생한다.

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -1,6 +1,9 @@
 package baseball;
 
 public class Number {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final String OUT_OF_RANGE_STR = "range should be 1-9, input: [%d]";
 
     private final int number;
 
@@ -10,6 +13,10 @@ public class Number {
 
     public static Number of(String str) {
         int number = Integer.parseInt(str);
+        if (number > MAX_NUMBER || number < MIN_NUMBER) {
+            throw new IllegalArgumentException(String.format(OUT_OF_RANGE_STR, number));
+        }
         return new Number(number);
     }
+
 }
