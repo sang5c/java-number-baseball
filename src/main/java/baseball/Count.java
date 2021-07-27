829173ecf6ed3c4d07c4061bd08b5d1d9fafa836
sang5c <skah321@gmail.com>
2021-07-27 10:15:05 +0900

    feat(Count): 0-3 범위가 아니면 Exception이 발생한다.

--- src/main/java/baseball/Count.java
+++ src/main/java/baseball/Count.java
@@ -1,10 +1,21 @@
 package baseball;
 
 public class Count {
+    private static final int MIN_COUNT = 0;
+    private static final int MAX_COUNT = 3;
+    private static final String COUNT_RANGE_EXCEPTION_STR = "strike, ball range %d-%d, input: [%d]";
+
     private int count;
 
     public Count(int count) {
+        validateRange(count);
         this.count = count;
+    }
+
+    private void validateRange(int count) {
+        if (count < MIN_COUNT || count > MAX_COUNT) {
+            throw new IllegalArgumentException(String.format(COUNT_RANGE_EXCEPTION_STR, MIN_COUNT, MAX_COUNT, count));
+        }
     }
 
 }
