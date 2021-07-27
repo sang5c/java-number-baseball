8609e45e53cb464e6dfd576358b5c26a29a0c134
sang5c <skah321@gmail.com>
2021-07-27 21:21:26 +0900

    feat(Numbers): 길이가 3이 아니면 Exception

--- src/main/java/baseball/Numbers.java
+++ src/main/java/baseball/Numbers.java
@@ -4,14 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Numbers {
+    public static final int NUMBERS_LENGTH = 3;
+    private static final String INVALID_LENGTH_STR = "length should be 3, input: [%d]";
 
     private final List<Number> numbers;
 
-    public Numbers() {
+    private Numbers() {
         this.numbers = new ArrayList<>();
     }
 
     public static Numbers of(String str) {
+        if (str.length() != NUMBERS_LENGTH) {
+            throw new IllegalArgumentException(String.format(INVALID_LENGTH_STR, str.length()));
+        }
         return new Numbers();
     }
 
